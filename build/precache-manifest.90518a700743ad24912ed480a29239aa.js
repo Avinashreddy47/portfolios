c101cc40d9747e445bd39703015a015bbf30c641
Avinash Reddy Sallagonda <40892223+Avinashreddy47@users.noreply.github.com>
2024-02-08 03:13:11 -0600

    Add files via upload

---
+++ build/precache-manifest.90518a700743ad24912ed480a29239aa.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06054331c5738fd94318fea819f67dcc",
+    "url": "/portfolios/build/index.html"
+  },
+  {
+    "revision": "9c4884d98b5b2b724bba",
+    "url": "/portfolios/build/static/css/2.01d557b6.chunk.css"
+  },
+  {
+    "revision": "9785d071a71adf78829f",
+    "url": "/portfolios/build/static/css/main.6ebb99c1.chunk.css"
+  },
+  {
+    "revision": "9c4884d98b5b2b724bba",
+    "url": "/portfolios/build/static/js/2.d02f73f8.chunk.js"
+  },
+  {
+    "revision": "32f24742585caeac27f9ecc59ba74e5d",
+    "url": "/portfolios/build/static/js/2.d02f73f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9785d071a71adf78829f",
+    "url": "/portfolios/build/static/js/main.dcbfa129.chunk.js"
+  },
+  {
+    "revision": "a760cefee254d6a30078",
+    "url": "/portfolios/build/static/js/runtime-main.a52b401d.js"
+  },
+  {
+    "revision": "67af05e12311bac06327b43ca31e166b",
+    "url": "/portfolios/build/static/media/earth.67af05e1.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/portfolios/build/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/portfolios/build/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/portfolios/build/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/portfolios/build/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/portfolios/build/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "0b37f43494ce5a35f3e1575612048750",
+    "url": "/portfolios/build/static/media/image4.0b37f434.jpg"
+  },
+  {
+    "revision": "8d16c4aff0e7bd3db1b8bab9619aea9c",
+    "url": "/portfolios/build/static/media/image5.8d16c4af.jpg"
+  },
+  {
+    "revision": "62b7cc7591147260c113823ded8f6901",
+    "url": "/portfolios/build/static/media/image6.62b7cc75.jpg"
+  },
+  {
+    "revision": "d62c314aa4f03d44ad18f1aca4cbd943",
+    "url": "/portfolios/build/static/media/img.d62c314a.jpg"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/portfolios/build/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/portfolios/build/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/portfolios/build/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "621bd386841f74e0053cb8e67f8a0604",
+    "url": "/portfolios/build/static/media/ionicons.621bd386.svg"
+  },
+  {
+    "revision": "a82ab1cd3c28c0a07a4932d57176cb88",
+    "url": "/portfolios/build/static/media/male.a82ab1cd.png"
+  },
+  {
+    "revision": "3ec3891b37f8b0765b554f10806f0b3f",
+    "url": "/portfolios/build/static/media/male1.3ec3891b.png"
+  },
+  {
+    "revision": "bd80794fddbffb4031fab537cc898680",
+    "url": "/portfolios/build/static/media/myImage.bd80794f.png"
+  }
+]);
